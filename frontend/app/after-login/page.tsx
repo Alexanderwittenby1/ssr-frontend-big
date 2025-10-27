6aeb6bdc6687f34497d2e98004be53d94e7b73de
Alexander <allewi@live.se>
2025-10-27 16:55:42 +0100

    fffa

--- frontend/app/after-login/page.tsx
+++ frontend/app/after-login/page.tsx
@@ -2,18 +2,24 @@
 
 import { useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 function AfterLoginInner() {
   const router = useRouter();
+  const { data: session } = useSession();
   const params = useSearchParams();
   const callbackUrl = params.get("callbackUrl") || "/";
 
   useEffect(() => {
+    if (!session?.jwt) return; // Vänta tills JWT finns
+
     async function linkUser() {
-      const res = await fetch(`/api/backend/auth/oauth-link`,
-      { 
-      method: "POST" ,
-      credentials: "include",
+      const res = await fetch(`/api/backend/auth/oauth-link`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${session.jwt}`,
+          "Content-Type": "application/json",
+        },
       });
 
       if (!res.ok) {
@@ -24,7 +30,7 @@ function AfterLoginInner() {
     }
 
     linkUser();
-  }, [callbackUrl, router]);
+  }, [session, callbackUrl, router]);
 
   return (
     <p className="flex justify-center items-center h-screen">
